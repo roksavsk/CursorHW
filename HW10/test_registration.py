1fd97e4e209e89813f974b373fc91875d5075e7b
Oksana <79321579+roksavsk@users.noreply.github.com>
2021-04-16 13:40:35 +0300

    Update test_registration.py

--- HW10/test_registration.py
+++ HW10/test_registration.py
@@ -10,32 +10,32 @@ class TestRegistration(unittest.TestCase):
         with self.assertRaises(ErrorName):
             self.user.register("Yv*nne", "yvonne@gmail.com", "poiuyt098")
             self.user.register("Yv&nne?", "yvonne@gmail.com", "poiuyt098")
-        self.user.register("Yvonne", "yvonne@gmail.com", "poiuyt098")
+        self.user.register("Yvonne", "yvonne@gmail.com", "Poiuyt098")
         with self.assertRaises(EmailError):
             self.user.register("Estella", "estella_m@gmail.com", "zxcvb987")
             self.user.register("Estella", "estellam@@gmail.com", "zxcvb987")
-        self.user.register("Estella", "estellam@gmail.com", "zxcvb987")
+        self.user.register("Estella", "estellam@gmail.com", "Zxcvb987")
         with self.assertRaises(LengthEmailError):
             self.user.register("Iris", "i@mail.com", "asdfg123")
             self.user.register("Iris", "ir@mail.com", "asdfg123")
-        self.user.register("Iris", "iris@gmail.com", "asdfg123")
+        self.user.register("Iris", "iris@gmail.com", "Asdfg123")
         with self.assertRaises(UserAlreadyRegistered):
             self.user.register("Lucas", "jacob@gmail.com", "123qwerty")
             self.user.register("Lucas", "jacob@gmail.com", "zxcvb456")
-        self.user.register("Lucas", "jacob2@gmail.com", "zxcvb456")
+        self.user.register("Lucas", "jacob2@gmail.com", "Zxcvb456")
         with self.assertRaises(PasswordLengthError):
             self.user.register("Grace", "grace@gmail.com", "qaz5")
-        self.user.register("Grace", "grace@gmail.com", "qazwsx579")
+        self.user.register("Grace", "grace@gmail.com", "Qazwsx579")
         with self.assertRaises(PasswordError):
             self.user.register("Quinn", "quinn@gmail.com", "qu!nn379")
             self.user.register("Quinn", "quinn@gmail.com", "qu!nn$%#379")
-        self.assertEqual(self.user.register("Quinn", "quinn@gmail.com", "quinn379"), 200)
+        self.assertEqual(self.user.register("Quinn", "quinn@gmail.com", "Quinn379"), 200)
 
     def test_login_in(self):
         with self.assertRaises(ErrorLogIn):
             self.user.login_in("jacob@gmail.com", "123qw")
-            self.user.login_in("quinn@gmail.com", "123qwerty")
-        self.user.login_in("jacob@gmail.com", "123qwerty")
+            self.user.login_in("quinn@gmail.com", "123Qwerty")
+        self.user.login_in("jacob@gmail.com", "123Qwerty")
 
 
     def tearDown(self) -> None:
